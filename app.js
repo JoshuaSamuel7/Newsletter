7b072d3788d0fa88beb623d207b4007da7404f18
joshua <joshua@gmail.com>
2024-08-30 20:20:14 +0530

    initial

--- app.js
+++ app.js
@@ -1,56 +1,60 @@
-const express=require("express");
-const bodyParser=require("body-parser");
-const request=require("request");
-const https=require("https");
-const app=express();
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static("public"));
-
-
-const apiKey="0080267197c35d41f194205da0d4b948-us21";
-
-app.listen(process.env.PORT || 3000,function(){
-    console.log("Server is Live");
-})
-app.get("/",function(req,res){
-    res.sendFile(__dirname+"/signup.html");
-});
-
-app.post("/",function(req,res){
-    var firstn=req.body.first;
-    var lastn=req.body.last;
-    var mail=req.body.email;
-    var data={
-        members:[
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const cors=require("cors")
+const app = express();
+app.use(bodyParser.json());
+app.use(cors(
+    {
+        origin:"http://localhost:3000"
+    }
+))
+app.post("/signup", function(req, res) {
+    const firstn = req.body.first;
+    const lastn = req.body.last;
+    const mail = req.body.email;
+    const add=req.body.address;
+    const phoneno=req.body.ph;
+    const data = {
+        members: [
             {
-                email_address:mail,
-                status:"subscribed",
-                merge_fields:{
-                    FNAME:firstn,
-                    LNAME:lastn
+                email_address: mail,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstn,
+                    LNAME: lastn,
+                    ADDRESS:add,
+                    PHONE:phoneno
                 }
             }
         ]
-    }
-    var url="https://us21.api.mailchimp.com/3.0/lists/bd24b69610";
-    var options={
-        method:"POST",
-        auth:"joshua:0080267197c35d41f194205da0d4b948-us21"
-    }
-    var jsonData=JSON.stringify(data);
-    const request=https.request(url,options,function(response){
-        response.on("data",function(data){
-            console.log(JSON.parse(data));
-            if(response.statusCode==200){
-                res.sendFile(__dirname+"/success.html");
-            }else{
-                res.sendFile(__dirname+"/failure.html");
-            }
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    const url = 'https://us21.api.mailchimp.com/3.0/lists/bd24b69610';
+    const options = {
+        method: 'POST',
+        headers: {
+            'Authorization': 'auth 0080267197c35d41f194205da0d4b948-us21',  
+            'Content-Type': 'application/json'
+        },
+        data: jsonData,
+        url
+    };
+
+    axios(options)
+        .then(response => {
+            console.log(response);
+            
+            res.json({ message: "Successfully subscribed!" });
+        })
+        .catch(error => {
+            console.error(error);
+            res.status(500).json({ message: "An error occurred while signing up." });
         });
-    });
-    app.post("/failure",function(req,res){
-        res.redirect("/");
-    })
-    request.write(jsonData);
-    request.end();
+});
+
+app.listen(8000, function() {
+    console.log("Server is running on port 8000");
 });
